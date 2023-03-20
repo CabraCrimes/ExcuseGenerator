da7845467530091c572e8c38214289c7bdc18ccf
CabraCrimes <benalexanderthegreat@gmail.com>
2023-03-20 19:29:33 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "kicked", "punched", "stabbed"];
+  let what = ["my homework", "the keys", "the car", "the president"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished working",
+    "during my lunch",
+    "while I was praying"
+  ];
+  //let whoExcuse = who[Math.floor(Math.random(who.length) * who.length)];
+  const excuseGenerator = () => {
+    let whoExcuse = Math.floor(Math.random() * who.length);
+    let actionExcuse = Math.floor(Math.random() * action.length);
+    let whatExcuse = Math.floor(Math.random() * what.length);
+    let whenExcuse = Math.floor(Math.random() * when.length);
+
+    return `${who[whoExcuse]} ${action[actionExcuse]} ${what[whatExcuse]} ${when[whenExcuse]}`;
+  };
+
+  console.log(excuseGenerator());
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
